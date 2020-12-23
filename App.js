a339d42a19f997c7f007bd571836220c58f10309
Yogesh <yogeshmore044@gmail.com>
2020-12-23 16:52:26 +0530

    State and Component and props

--- App.js
+++ App.js
@@ -1,12 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+
+  const [displayText, displayTextHandler] = useState('I am new Text');
+
+  const [counter, counterHandler] = useState(0);
+
+  const pressHandler = (cls) => {
+    displayTextHandler(cls.target.value);
+  }
+
+  const incrementHandler = () => {
+    counterHandler(counter + 1);
+
+    // let plus = counter = counter + 1;
+    // counterHandler(plus);
+  }
+
+  const decrementHandler = () => {
+    // let minus = counter = counter - 1;
+    counterHandler(counter - 1);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World <ShowName name="Apple" title="Developer" /></Text>
+      <Text><ShowName lastName="Kapoor" /></Text>
+      <TextInput style={styles.textInput} onKeyPress={(text) => {
+        pressHandler(text);
+      }} />
+      <Text>{displayText}</Text>
+      <Button title="+" onPress={() => {
+        incrementHandler()
+      }}></Button>
+      <TextInput style={styles.textInput} placeholder="The Number Will be display here" value={counter} />
+      <Button title="-" onPress={() => {
+        decrementHandler()
+      }}></Button>
+      <LoginForm />
+      <LoginForm />
+      <LoginForm />
+      <LoginForm />
+      <LoginForm />
+      <LoginForm />
+      <LoginForm />
+      <LoginForm />
+      <LoginForm />
+      <LoginForm />
+      <LoginForm />
+      <LoginForm />
       <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const ShowName = (props) => {
+  return (
+    <View>
+      <Text>My Name is {props.name} {props.lastName} {props.title}</Text>
+    </View>
+  );
+}
+
+const LoginForm = () => {
+  return (
+    <View>
+      <Text>I am Login Form</Text>
     </View>
   );
 }
@@ -18,4 +79,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInput: {
+    borderWidth: 2,
+    textAlign: 'center'
+  }
 });
